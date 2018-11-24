c736b4dfd5ecc04e36c599a64cb1170cc1c3bfaa
MorganeHenry <44085609+MorganeHenry@users.noreply.github.com>
2018-11-25 00:45:41 +0100

    Add files via upload

---
+++ element.java
@@ -0,0 +1,25 @@
+package Clipboard;
+
+
+public class element {
+	
+	double content;
+	
+	public boolean isNumber()
+	{
+		return false;
+	}
+	
+
+	public void display()
+	{
+		System.out.println(content);
+	}
+	
+	/*public double getContent()
+	{
+		return content;
+		
+	}*/
+	
+}
---
+++ elementInt.java
@@ -0,0 +1,23 @@
+package Clipboard;
+
+
+public class elementInt extends elementnumber{
+
+	private int content;
+	
+	public elementInt(int input)
+	{
+		content = input;
+	}
+	
+	public double getContent() {
+		// TODO Auto-generated method stub
+		return (double)content;
+	}
+	
+	public void display()
+	{
+		System.out.println(content);
+	}
+
+}
